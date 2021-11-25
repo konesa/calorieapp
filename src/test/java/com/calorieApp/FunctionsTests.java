ad68974169d61bef311ac15abed89112fadd2b01
konesa <esa.kontio@myy.haaga-helia.fi>
2021-11-26 00:33:56 +0200

    tests

--- src/test/java/com/calorieApp/FunctionsTests.java
+++ src/test/java/com/calorieApp/FunctionsTests.java
@@ -5,15 +5,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.calorieApp.domain.*;
 
 @ExtendWith(SpringExtension.class)
-@DataJpaTest
+@SpringBootTest
 
-public class FunctionsTests {
+class FunctionsTests {
+
+	@Autowired
+	UserFunctions functions;
+	@Autowired
+	UserRepository userRepo;
 
 	User user = new User("user", "user", "user2@user.com",
 			"$2a$10$T.fEvFa9UpgdacosOAUvaubtI/hPcGlEV2kMzAJTSLa70Zg72GHhK",
@@ -24,11 +29,6 @@ public class FunctionsTests {
 			"$2a$10$T.fEvFa9UpgdacosOAUvaubtI/hPcGlEV2kMzAJTSLa70Zg72GHhK", "ADMIN");
 
 	Meal meal = new Meal(1, "asd", 1, 1, 1, 1);
-
-	@Autowired
-	UserFunctions functions;
-	@Autowired
-	UserRepository userRepo;
 
 	@Test
 	public void registerUser() {
@@ -61,7 +61,7 @@ public class FunctionsTests {
 	public void deleteMeal() {
 		assertThat(functions.deleteMeal(meal.getId()) == true);
 	}
-	
+
 	@Test
 	public void deleteUser() {
 		assertThat(functions.deleteUser(user.getId()) == true);
