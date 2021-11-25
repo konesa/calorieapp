a41eecb75c6c332c2230a564fea83b3bb329f983
konesa <esa.kontio@myy.haaga-helia.fi>
2021-11-26 00:52:23 +0200

    tests

--- src/main/java/com/calorieApp/web/LoginController.java
+++ src/main/java/com/calorieApp/web/LoginController.java
@@ -1,21 +1,13 @@
 package com.calorieApp.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.calorieApp.domain.FunctionsInterface;
-import com.calorieApp.domain.UserRepository;
 
 @Controller
 public class LoginController {
-
-	@Autowired
-	FunctionsInterface functions;
-	@Autowired
-	UserRepository userRepo;
-
 	@RequestMapping(value = "/login")
 	public String loginController() {
+		System.out.println("made it");
 		return ("login");
 	}
 }
