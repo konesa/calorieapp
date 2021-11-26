cb600d2548598ea32522cc3275efbe0ef21da5c1
konesa <esa.kontio@myy.haaga-helia.fi>
2021-11-26 18:10:36 +0200

    tests

--- src/main/java/com/calorieApp/web/IndexController.java
+++ src/main/java/com/calorieApp/web/IndexController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.calorieApp.domain.Meal;
 import com.calorieApp.domain.UserFunctions;
@@ -26,7 +25,7 @@ public class IndexController {
 			return "index";
 		}
 	}
-	@RequestMapping(value = "/")
+	@GetMapping(value = "/")
 	public String indexCont2() {
 		return "index";
 	}
